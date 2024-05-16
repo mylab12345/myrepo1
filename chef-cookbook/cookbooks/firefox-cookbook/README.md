eb2a1a38e82e2a4edf72c8cbabab3963cf12996c
mayur <mayur@gmail.com>
2024-05-16 21:55:52 +0530

    recipe for chrome,firefox,https cookbook

---
+++ chef-cookbook/cookbooks/firefox-cookbook/README.md
@@ -0,0 +1,4 @@
+# firefox-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ chef-cookbook/cookbooks/firefox-cookbook/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: firefox-cookbook
+# Recipe:: default
+#
+# Copyright:: 2024, The Authors, All Rights Reserved.
